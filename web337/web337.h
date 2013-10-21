d1545f221f1fd86bcd271350775420914b7231f7
baoyu <baoyu@elex-tech.com>
2013-10-21 16:50:17 +0800

    append

--- web337/web337.h
+++ web337/web337.h
@@ -1,13 +1,3 @@
-//
-//  web337.h
-//  web337
-//
-//  Created by elex on 13-10-21.
-//  Copyright (c) 2013年 337. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface web337 : NSObject
-
-@end
+#import "ElxUser.h"
+#import "ElxError.h"
+#import "ElxWeb337.h"
